1920dbcd5a420a3ca30ed2d02b2061cd3fd6b198
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-02 21:43:44 -0400

    Remove Config stuff (use Core), remove accidental archapi reference
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- forge/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/forge/GenerationsUltraSpaceForge.java
+++ forge/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/forge/GenerationsUltraSpaceForge.java
@@ -9,7 +9,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 public class GenerationsUltraSpaceForge {
     public GenerationsUltraSpaceForge() {
         // Submit our event bus to let architectury register our content on the right time
-        EventBuses.registerModEventBus(GenerationsUltraSpace.MOD_ID, FMLJavaModLoadingContext.get().getModEventBus());
         GenerationsUltraSpace.init();
     }
 }
