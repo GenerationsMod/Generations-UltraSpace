641db15b19e46ff01f2ab5c9b310185f38343baa
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-08 22:30:21 -0400

    remove arch thingy
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- forge/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/forge/GenerationsUltraSpaceForge.java
+++ forge/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/forge/GenerationsUltraSpaceForge.java
@@ -1,6 +1,5 @@
 package generations.gg.generations.ultraspace.generationsultraspace.forge;
 
-import dev.architectury.platform.forge.EventBuses;
 import generations.gg.generations.ultraspace.generationsultraspace.GenerationsUltraSpace;
 import net.minecraftforge.fml.common.Mod;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
@@ -8,7 +7,6 @@ import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 @Mod(GenerationsUltraSpace.MOD_ID)
 public class GenerationsUltraSpaceForge {
     public GenerationsUltraSpaceForge() {
-        // Submit our event bus to let architectury register our content on the right time
         GenerationsUltraSpace.init();
     }
 }
