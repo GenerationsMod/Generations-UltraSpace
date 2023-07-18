0ea9466326af24232160e8bc5cc1280d8bb2298f
Joseph T. McQuigg <J.T.McQuigg12@gmail.com>
2023-07-18 09:07:02 -0400

    add Config
    Signed-off-by: Joseph T. McQuigg <J.T.McQuigg12@gmail.com>

--- common/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/GenerationsUltraSpace.java
+++ common/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/GenerationsUltraSpace.java
@@ -1,8 +1,19 @@
 package generations.gg.generations.ultraspace.generationsultraspace;
 
+import generations.gg.generations.core.generationscore.ConfigLoader;
+import generations.gg.generations.ultraspace.generationsultraspace.config.Config;
+
 public class GenerationsUltraSpace {
+
+    /** The mod ID for the Generations-UltraSpace mod. */
     public static final String MOD_ID = "generations_ultraspace";
+
+    /** The config for the Generations-UltraSpace mod. */
+    public static Config CONFIG;
+
+
     public static void init() {
+        CONFIG = ConfigLoader.loaderConfig(Config.class, MOD_ID, "config");
         UltraSpaceDimension.registerDimension();
     }
 }
---
+++ common/src/main/java/generations/gg/generations/ultraspace/generationsultraspace/config/Config.java
@@ -0,0 +1,12 @@
+package generations.gg.generations.ultraspace.generationsultraspace.config;
+
+public class Config {
+
+    public UltraSpaceConfig ultraspaceconfig = new UltraSpaceConfig();
+
+    public Config() {}
+
+    public static class UltraSpaceConfig {
+
+    }
+}
